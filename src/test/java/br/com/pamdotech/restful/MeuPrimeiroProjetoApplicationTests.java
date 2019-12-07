fcfaabfe421132114c145e99ea9aa56ab690476c
guidoaspaixao <guido.aspaixao@gmail.com>
2019-12-06 22:38:30 -0300

    Adicionando Novo Projeto ao Git

---
+++ src/test/java/br/com/pamdotech/restful/MeuPrimeiroProjetoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pamdotech.restful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuPrimeiroProjetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
