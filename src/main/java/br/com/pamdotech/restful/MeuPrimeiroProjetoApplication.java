e63f8989299c7f886158a130ee06681a826801cd
guidoaspaixao <guido.aspaixao@gmail.com>
2019-12-09 14:44:24 -0300

    Adicionado mecanismo de cache para consulta a componentes do Spring

--- src/main/java/br/com/pamdotech/restful/MeuPrimeiroProjetoApplication.java
+++ src/main/java/br/com/pamdotech/restful/MeuPrimeiroProjetoApplication.java
@@ -2,8 +2,10 @@ package br.com.pamdotech.restful;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class MeuPrimeiroProjetoApplication {
 
 	public static void main(String[] args) {
--- src/main/java/br/com/pamdotech/restful/controllers/InfoUrlController.java
+++ src/main/java/br/com/pamdotech/restful/controllers/InfoUrlController.java
@@ -1,19 +1,33 @@
 package br.com.pamdotech.restful.controllers;
 
+
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
 @RequestMapping("/api/infourl")
 public class InfoUrlController {
 
+	private static final Logger log = LoggerFactory.getLogger(InfoUrlController.class);
+	
 	@GetMapping(value = "/{nome}")
 	@PreAuthorize("hasAnyRole('ADMIN')")
 	public String exemplo(@PathVariable("nome") String nome) {
 		 return "Olá " + nome;
+	}
+	
+	
+	@Cacheable("exemploCache")
+	@GetMapping(value = "/cache")
+	public String exemploCache() {
+		 log.info("### Executando o oontroller...");
+		 return "Obtendo informação em cache ";
 	}
 	
 }
